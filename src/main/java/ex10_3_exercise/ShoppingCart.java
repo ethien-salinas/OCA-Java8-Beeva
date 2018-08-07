5582532de834a9f0d1c4757055f3787030279f33
Ethien Salinas <ethien.salinas@gmail.com>
2018-08-06 23:05:35 -0500

    [DELETE] unnecessary folders

--- src/main/java/ex10_3_exercise/ShoppingCart.java
+++
@@ -1,9 +0,0 @@
-package ex10_3_exercise;
-
-public class ShoppingCart {
-
-    public static void main(String args[]) {
-        Order order = new Order("Rick Wilson", 910.00, "VA", Order.NONPROFIT);
-        System.out.println("Discount is: " + order.getDiscount());
-    }
-}
