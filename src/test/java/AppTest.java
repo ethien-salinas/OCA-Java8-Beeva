a29e371e9962da823548bf73ed0ac0eb10dfeff4
Ethien Salinas <ethien.salinas@gmail.com>
2018-08-06 22:15:50 -0500

    [ADD] ternary operator test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.function.ToIntBiFunction;
+
 public class AppTest {
     @Test
     public void testAppHasAGreeting() {
@@ -12,7 +14,7 @@ public class AppTest {
     }
 
     @Test
-    void testPrecedence(){
+    void testPrecedence() {
         int c = 25 - 5 * 4 / 2 - 10 + 4;
         int d = (((25 - 5) * 4) / (2 - 10)) + 4;
         assertThat(c).isNotEqualTo(34);
@@ -20,5 +22,26 @@ public class AppTest {
 
         assertThat(d).isNotEqualTo(9);
         assertThat(d).isEqualTo(-6);
+    }
+
+    @Test
+    void testTernary() {
+        // old school way
+        int x0 = 4, y0 = 9;
+        if (y0 / x0 < 3) {
+            x0 += y0;
+        } else {
+            x0 *= y0;
+        }
+        assertThat(x0).isEqualTo(13);
+
+        // with ternary operator
+        int x1 = 4, y1 = 9;
+        x1 = (y1 / x1 < 3) ? x1 + y1 : x1 * y1;
+        assertThat(x1).isEqualTo(13);
+
+        ToIntBiFunction<Integer, Integer> elvisLambda = (x, y) -> (y / x < 3) ? x + y : x * y;
+        assertThat(elvisLambda.applyAsInt(4, 9)).isEqualTo(13);
+
     }
 }
